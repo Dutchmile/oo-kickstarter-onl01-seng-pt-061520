9b0893fb05e3a2f1d3499bc56d3823248a281ca4
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 16:56:38 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 class Backer
 
-attr_accessor :backed_projects
-attr_reader :name
+  attr_accessor :backed_projects
+  attr_reader :name
 
-def initialize(name)
-  @name = name
-  @backed_projects = []
-end
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
-def back_project(project)
-  @backed_projects << project
-
+  def back_project(project)
+    @backed_projects << project
+  end
 
 end
